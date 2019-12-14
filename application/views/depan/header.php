dab67cc14c4cd0fefbb5c6011c375f22141c6ca3
rifkiandriyanto <rifkiandriyanto@users.noreply.github.com>
2019-12-14 07:05:33 +0700

    mschool

---
+++ application/views/depan/header.php
@@ -0,0 +1,69 @@
+    <div class="header-topbar">
+        <div class="container">
+                        <div class="row">
+                <div class="col-xs-6 col-sm-8 col-md-10">
+                    <div class="header-top_address">
+                        <div class="header-top_list">
+                            <span class="icon-phone"></span>0812 1454 1744
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-envelope-open"></span>mail@smpitnba.com
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-location-pin"></span>Jalan Tomik RT.14/RW.03, Desa Jalaksana, Kecamatan Jalaksana, Kabupaten Kuningan
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div data-toggle="affix" style="border-bottom:solid 1px #f2f2f2;">
+        <div class="container nav-menu2">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
+                        <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                            <span class="icon-menu"></span>
+                        </button>
+                        <a href="<?php echo site_url('');?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="100px;" src="<?php echo base_url().'theme/images/logo-dark.png'?>"></a>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('');?>">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('about');?>">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('guru');?>">Guru</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('siswa');?>">Siswa</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('blog');?>">Blog</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('pengumuman');?>">Pengumuman</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('agenda');?>">Agenda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('download');?>">Download</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('galeri');?>">Gallery</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('contact');?>">Contact</a>
+                                </li>
+                          </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+      <section>
+</section>
---
+++ application/views/depan/v_about.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About</title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/icon.png'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick-theme.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+</head>
+
+<body>
+
+        <!--============================= HEADER =============================-->
+            <?php $this->load->view('depan/header'); ?>
+         <!--//END HEADER -->
+
+
+<!--//END ABOUT IMAGE -->
+<!--============================= WELCOME TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Kata Sambutan</h2>
+                <br><br><p>Pendidikan menjadi kebutuhan primer di zaman ini, menjadi bekal masa depan bagi setiap manusia. Tuntutan zaman yang tidak bisa diabaikan oleh manusia, namun tentunya pendidikan yang diperoleh bukan hanya sekedar pendidikan yang berorientasi pada pengetahuan akal saja tetapi juga pendidikan yang harus bisa dikolaborasikan dengan pendidikan agama. </p>
+<p>Kedua pendidikan dizaman sebelumnya dipisahkan karena dianggap dua pendidikan yang sulit disatukan karena memiliki tujuan yang berbeda, pendidikan yang satu berorientasi pada keberhasilan duniawi dan pendidikan yang satunya berorientasi pada akhirat. </p>
+<p>Pemikiran ini mulai terbantahkan seiring dengan perkembangan zaman dan menyadari bahwa dua pendidikan ini harus bisa berjalan beriringan, harus bisa saling memberikan manfaat. </p>
+<p>Keterbukaan pemikiran, pengetahuan serta kenyataan yang saat ini terus terjadi dan memiliki nilai kemerosotan sosial apalagi ketika itu dihadapkan pada sisi agama. Banyak orang yang memiliki kecerdasan tinggi tetapi dia tidak memiliki nilai religiusitas yang mumpuni, sehingga pada akhirnya seseorang yang memiliki kecerdasan tinggi menjadikan dirinya tidak mengenal Tuhannya, yang diketahui hanyalah keberhasilan yang didapat akibat dari kecerdasan otaknya saja bukan karena didasarkan dari anugerah yang Allah berikan pada dirinya. Maka tidak ayal di zaman sekarang ini banyak ditemukan orang yang takabur, sangat mendewakan akal pikiran sehingga semakin tidak mengenal Tuhannya. </p>
+<p>Maka tentu saja pendidikan yang mampu menyatukan kedua sistem ini menjadi salah satu solusi pilihan cerdas, karena akan didapatkan kecerdasan akal pikiran dan juga tingkat reigiusitas yang tinggi. Keuntungannya yang paling mendasar adalah mampu menyatukan kemampuan akal pikiran dan sisi religiusitasnya. Ini yang akan menjadikan seseorang menjadi <strong>Insan Kamil</strong>. Memberikan manfaat untuk dunia dan akhirat, untuk dirinya dan orang lain. </p>
+<p><strong> SMP IT & PONPES Nur Barokah Al-Wadani </strong> yang sampai saat ini konsisten dan akan akan terus konsisten dengan menyatukan kedua sistem ini. Harapan kami adalah mampu melahirkan generasi penerus bangsa yang cerdas namun tidak pernah akan melupakan jati dirinya sebagai<strong> Hamba</strong>.</p>
+
+                </div>
+                <div class="col-md-5">
+                    <img src="<?php echo base_url().'theme/images/welcome-img.jpg'?>" class="img-fluid" alt="#">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END WELCOME TITLE -->
+    <!--============================= TESTIMONIAL =============================-->
+    <section class="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Testimonial</h2>
+                </div>
+                <div class="col-md-12">
+                    <div class="single-item">
+                        <div class="quote">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            <p class="quote_text">MSCHOOL benar-benar mengagumkan. Saya sangat senang bisa bergabung dengan MSCHOOL dan menjadi siswa terbaik tahun 2018.</p>
+                            <div class="testi-img_block">
+                                <img src="<?php echo base_url().'theme/images/student-1.png'?>" class="img-fluid" alt="#">
+                                <p><span>Hernandez Alvaro</span>Siswa Terbaik 2018</p>
+                            </div>
+                        </div>
+                        <div class="quote">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            <p class="quote_text">MSCHOOL benar-benar mengagumkan. Saya sangat senang bisa bergabung dengan MSCHOOL dan menjadi siswa terbaik tahun 2017. </p>
+                            <div class="testi-img_block">
+                                <img src="<?php echo base_url().'theme/images/student-2.png'?>" class="img-fluid" alt="#">
+                                <p><span>Elanoar Rigby</span>Siswa Terbaik 2017</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END TESTIMONIAL -->
+
+        <!--============================= FOOTER =============================-->
+                <?php $this->load->view('depan/footer'); ?>
+                <!--//END FOOTER -->
+
+                <!-- jQuery, Bootstrap JS. -->
+                <script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tether.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+                <!-- Plugins -->
+                <script src="<?php echo base_url().'theme/js/slick.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/waypoints.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/counterup.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/owl.carousel.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/validate.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tweetie.min.js'?>"></script>
+                <!-- Subscribe -->
+                <script src="<?php echo base_url().'theme/js/subscribe.js'?>"></script>
+                <!-- Script JS -->
+                <script src="<?php echo base_url().'theme/js/script.js'?>"></script>
+            </body>
+
+            </html>
